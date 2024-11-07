0519c88dc8386479b5c3e255258caaa6fa2c6223
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2024-11-07 08:53:09 +0100

    Add a new selector widget for printings.

--- cockatrice/src/client/tabs/tab_deck_editor.h
+++ cockatrice/src/client/tabs/tab_deck_editor.h
@@ -57,6 +57,8 @@ private slots:
     void updateCardInfoRight(const QModelIndex &current, const QModelIndex &previous);
     void updateSearch(const QString &search);
     void databaseCustomMenu(QPoint point);
+    void decklistCustomMenu(QPoint point);
+    void createPrintingSelector(CardInfoPtr selectedCard);
 
     void actNewDeck();
     void actLoadDeck();
